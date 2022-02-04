0d6d74e9ef314b9177ab23fb995662dc369fd30a
Rafael Ferreira <rafaelcardoso990@gmail.com>
2022-02-04 13:10:43 -0300

    Condição quantidade de cartas

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let 
+function iniciar(){
+    cardNumbers = parseInt(prompt("Com quantas cartas você quer jogar? Insira um número par entre 4 e 14."));
+    while((cardNumbers % 2 !== 0) || (cardNumbers < 4) || (cardNumbers > 14)){
+        cardNumbers = parseInt(prompt("Escolha um número par entre 4 e 14."));
+    }
+}
+
+iniciar();
